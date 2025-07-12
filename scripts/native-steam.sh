49e370dff32ab9deb21f2f2b945c4fe4c5e97dd0
Ryan Farthing <ryan@ryanpf.com>
2025-07-12 17:06:04 -0500

    update wine asio paths from guide

--- scripts/native-steam.sh
+++ scripts/native-steam.sh
@@ -14,14 +14,10 @@ exit_msg() {
 # sets a few distro-specific variables. I'm assuming they only differ by parent-distro.
 if [ -f "$(which apt)" ]; then
 	dist=deb
-	x32dll=/usr/lib/i386-linux-gnu/wine/wineasio.dll
-	x32so=/usr/lib/i386-linux-gnu/wine/wineasio.dll.so
-	x64dll=/usr/lib/x86_64-linux-gnu/wine/wineasio.dll
-	x64so=/usr/lib/x86_64-linux-gnu/wine/wineasio.dll.so
+	x64dll=/usr/lib/x86_64-linux-gnu/wine/wineasio64.dll
+	x64so=/usr/lib/x86_64-linux-gnu/wine/wineasio64.dll.so
 elif [ -f "$(which pacman)" ]; then
 	dist=arch
-	x32dll=/usr/lib32/wine/i386-windows/wineasio.dll
-	x32so=/usr/lib32/wine/i386-unix/wineasio.dll.so
 	x64dll=/usr/lib/wine/x86_64-windows/wineasio.dll
 	x64so=/usr/lib/wine/x86_64-unix/wineasio.dll.so
 else
@@ -63,6 +59,7 @@ fi
 
 prefix="$game/../../compatdata/221680"
 
+## TODO read proton path from steam config
 echo "Please specify the root of your Proton installation"
 echo "that you want to use. It needs to be Proton,"
 echo "otherwise the game can't find Steam"
@@ -92,9 +89,11 @@ case $dist in
 	"deb")
 		echo "We will need to add an apt repo, to install necessary software"
 		## ask, or check, if this already exists
-		sudo apt-get install apt-transport-https gpgv
-		wget https://launchpad.net/~kxstudio-debian/+archive/kxstudio/+files/kxstudio-repos_10.0.3_all.deb
-		sudo dpkg -i kxstudio-repos_10.0.3_all.deb
+		sudo apt-get install apt-transport-https gpgv wget
+		# https://kx.studio/Repositories 2025-07-12
+		wget https://launchpad.net/~kxstudio-debian/+archive/kxstudio/+files/kxstudio-repos_11.2.0_all.deb -P /tmp
+		sudo dpkg -i /tmp/kxstudio-repos_11.2.0_all.deb
+
 		sudo apt update
 		sudo apt install cadence carla wineasio jackd2
 		;;
@@ -128,12 +127,10 @@ sudo usermod -aG audio $USER
 sudo usermod -aG realtime $USER
 
 # installs wineasio in proton
-cp "$x32dll" "$runner/lib/wine/i386-windows/wineasio.dll"
-cp "$x32so" "$runner/lib/wine/i386-unix/wineasio.dll.so"
-cp "$x64dll" "$runner/lib64/wine/x86_64-windows/wineasio.dll"
-cp "$x64so" "$runner/lib64/wine/x86_64-unix/wineasio.dll"
+cp "$x64dll" "$runner/lib/wine/x86_64-windows/wineasio64.dll"
+cp "$x64so" "$runner/lib/wine/x86_64-unix/wineasio64.dll.so"
 
-mv "$game/../../compatdata/221680" "$game/../../compatdata/221680.bak" # backs up an already existing prefix, makes place for a clean new one
+mv $prefix "$prefix.bak" # backs up an already existing prefix, makes place for a clean new one
 
 # tell user to close game once you see the loading screen
 echo
@@ -142,12 +139,15 @@ echo "The game will now start."
 echo "When the game opens and starts"
 echo "loading, you can close it again."
 echo "================================"
+echo
+## TODO this didn't pause execution
+read -p "Press enter to open the game"
 xdg-open steam://rungameid/221680 #run game
 
 echo
-read -p "Press enter to continue"
+read -p "Press enter after closing the game"
 
-WINEPREFIX="$prefix" regsvr32 /usr/lib32/wine/i386-windows/wineasio.dll # register wineasio in prefix
+WINEPREFIX="$prefix" wineasio-register # register wineasio in prefix
 
 # download RS_ASIO latest release, unpack to correct location, insert "WineASIO"
 wget https://github.com/mdias/rs_asio/releases/download/v0.5.7/release-0.5.7.zip -O RS_ASIO.zip
@@ -180,6 +180,7 @@ echo "and permissions are reloaded."
 echo "==============================================="
 echo
 
+## TODO configure JACK silently
 cadence
 
 exit 0
